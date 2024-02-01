c1e4eebb9ce4950f5a816c10981ddd5ccc8a0dc1
skrilexxx <admin.nuklear@seznam.cz>
2024-02-01 08:16:38 +0100

    failed page

--- carrental/src/routes/carDetails/[carId]/checkout/failed/+page.svelte
+++ carrental/src/routes/carDetails/[carId]/checkout/failed/+page.svelte
@@ -9,10 +9,28 @@
 
     <div class="text">
         <p>We're sorry to inform you that there was an issue processing your reservation request with Brno Car Rentals. We understand the importance of your travel plans, and we apologize for any inconvenience caused.</p>
-    </div>
 
-    <div class="orderDetails">
-        
+        <div class="whatNow">
+            <p class="title">Possible Reasons:</p>
+            <p class="head">1. Payment Issue:</p>
+            <p class="dot">There may be an issue with the payment details provided. Please ensure that the payment information is accurate and try again.</p>
+            <p class="head">2. Technical Glitch:</p>
+            <p class="dot">Occasionally, technical issues can affect the reservation process. Please try again later or contact our customer support team for assistance.</p>
+            <p class="head">3. Unavailable Vehicle:</p>
+            <p class="dot">The selected car type may not be available for the requested dates. Consider choosing an alternative car type or adjusting your pick-up and drop-off dates.</p>
+        </div>
+
+        <div class="whatNow">
+            <p class="title">What to Do Next:</p>
+            <p class="head">1. Review Information:</p>
+            <p class="dot">Double-check the information entered during the reservation process to ensure accuracy.</p>
+            <p class="head">2. Payment Details:</p>
+            <p class="dot">If the issue is related to payment, verify that the payment details provided are correct, and attempt the reservation again.</p>
+            <p class="head">3. Contact Customer Support:</p>
+            <p class="dot">If you continue to experience issues, please contact our customer support team at [Your Customer Support Email or Phone Number] for further assistance.</p>
+        </div>
+
+        <p>We apologize for any inconvenience this may have caused and appreciate your understanding. Our team is committed to resolving any issues promptly to ensure you have a seamless car rental experience.</p>
     </div>
 
 </div>
@@ -31,7 +49,41 @@
 }
 
 .text {
+    display: flex;
+    flex-direction: column;
     width: 80vw;
+    align-items: center;
+    font-size: 18px;
+}
+
+.whatNow {
+    width: 95%;
+    margin-top: 20px;
+}
+
+.whatNow p {
+    width: 95%;
+    text-align: left;
+}
+
+.dot {
+    margin-left: 10px;
+    margin-bottom: 15px;
+    margin-top: 0px;
+    font-size: 16px;
+}
+
+.dot::before {
+    content: "•";
+    font-weight: bold;
+    margin-right: 0.5rem;
+    margin-left: 10px;
+}
+
+.head {
+    font-weight: 600;
+    margin: 0;
+    font-size: 16px;
 }
 
 </style>
