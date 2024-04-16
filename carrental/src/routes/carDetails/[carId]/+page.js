e2947226f90c353618de3b172bf02cfe569b48b4
Adam Kopřiva <Admin.Nuklear@seznam.cz>
2024-04-16 22:36:33 +0200

    fixed fetch routes, hope this will work

--- carrental/src/routes/carDetails/[carId]/+page.js
+++ carrental/src/routes/carDetails/[carId]/+page.js
@@ -1,8 +1,15 @@
 
+let apiUrlCars = 'http://localhost:5173/api/getCars/';
+
+if (import.meta.env.VITE_VERCEL_ENV === 'production') {
+    apiUrlCars = 'https://127.0.0.1/api/getCars/';
+}
+
+
 export const load = async (loadEvent) => {
     const {fetch} = loadEvent;
     const carId = loadEvent.params.carId;
-    const response = await fetch('http://localhost:5173/api/getCars/' + carId );
+    const response = await fetch(`${apiUrlCars}${carId}`);
 
     if (response.ok) {
         return {
--- carrental/src/routes/carList/+page.js
+++ carrental/src/routes/carList/+page.js
@@ -1,7 +1,18 @@
+
+let apiUrlCars = 'http://localhost:5173/api/getCars/';
+let apiUrlFilters = 'http://localhost:5173/api/getFilters/';
+
+if (import.meta.env.VITE_VERCEL_ENV === 'production') {
+    apiUrlCars = 'https://127.0.0.1/api/getCars/';
+    apiUrlFilters = 'https://127.0.0.1/api/getFilters/';
+}
+
+
+
 export const load = async (loadEvent) => {
     const {fetch} = loadEvent;
-    const responseCars = await fetch('http://localhost:5173/api/getCars');
-    const responseFilters = await fetch('http://localhost:5173/api/getFilters');
+    const responseCars = await fetch(`${apiUrlCars}`);
+    const responseFilters = await fetch(`${apiUrlFilters}`);
 
 
     if (responseCars.ok && responseFilters.ok) {
